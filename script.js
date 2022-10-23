3d0e3f3559788817fdb4cb6d2cd5cf64430275c3
lkkrd <leshkord@gmail.com>
2022-10-23 15:20:42 +0200

    Funkcjonalna stronka

---
+++ script.js
@@ -0,0 +1,30 @@
+const colors = ["green", "blue", "#8a00c2", "#a000c8", "#b100cd"];
+const btn = document.getElementById("button");
+const color = document.querySelector(".color");
+const root = document.querySelector(":root");
+
+btn.addEventListener("click", function () {
+  window.colorNums = getRandomNumber();
+  console.log(window.colorNums);
+  changeColor();
+});
+
+//funkcja losuje tabelę z trzema numerami 0-255
+function getRandomNumber() {
+  let color = [];
+  for (i = 0; i < 3; i++) {
+    color[i] = Math.floor(Math.random() * 256);
+  }
+
+  return color;
+}
+
+//funkcja zmienia kolor
+function changeColor() {
+  console.log(
+    `red: ${window.colorNums[0]} green: ${window.colorNums[1]} blue: ${window.colorNums[2]}`
+  );
+  root.style.setProperty("--col0", window.colorNums[0]);
+  root.style.setProperty("--col1", window.colorNums[1]);
+  root.style.setProperty("--col2", window.colorNums[2]);
+}
